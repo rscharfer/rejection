4a18c2889ec310a00b9097b5695b01b9b37b342e
Ryan Scharfer <ryan.scharfer@finanzcheck.de>
2020-02-10 08:03:45 +0100

    created ADD_QUESTION action creator, reducer handles ADD_QUESTION actions, tests for both

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,33 @@
-const reducer = (state = []) => 0;
+import cuid from "cuid";
 
-export { reducer };
+
+const reducer = (state = [], action = {}) => {
+  switch (action.type) {
+    case addQuestion.type:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
+
+// addQuestion action creator
+const addQuestion = ({
+  timestamp = Date.now(),
+  id = cuid(),
+  askee,
+  question,
+  status
+}) => ({
+  type: addQuestion.type,
+  payload: {
+    timestamp,
+    id,
+    askee,
+    question,
+    status
+  }
+});
+
+addQuestion.type = "ADD_QUESTION";
+
+export { reducer, addQuestion };
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,13 +1,34 @@
 import { describe } from 'riteway'
 
-// import { reducer } from './reducer.js'
+import { reducer, addQuestion } from './reducer.js'
 
+const createState = (...questions) => questions
+
+const createQuestion = ({ id = 12344556, timestamp = 909034, askee, question, status}) => ({ 
+  timestamp,
+  id,
+  askee,
+  question,
+  status
+})
 
 describe('reducer()', async assert=>{
   assert({
     given: 'no arguments',
     should: 'return an empty array',
-    actual: 1,
-    expected: 1
+    actual: reducer(),
+    expected: []
+  })
+  assert({
+    given: 'ADD_QUESTION action obect',
+    should: 'return the state with that question added',
+    actual: reducer(reducer(), addQuestion({
+      id: 12344556,
+      timestamp: 909034,
+      question: 'Can I have a raise',
+      askee: 'boss',
+      status: 'yes'
+    })),
+    expected: createState(createQuestion({ askee: 'boss', question:'Can I have a raise', status:'yes'}))
   })
 })
