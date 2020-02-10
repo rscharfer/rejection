5ca752c1e5ad1eca40ed4fe1e433ab8f3c7457ad
Ryan Scharfer <ryan.scharfer@finanzcheck.de>
2020-02-10 07:37:48 +0100

    set up first passing tets

---
+++ src/reducer.test.js
@@ -0,0 +1,13 @@
+import { describe } from 'riteway'
+
+// import { reducer } from './reducer.js'
+
+
+describe('reducer()', async assert=>{
+  assert({
+    given: 'no arguments',
+    should: 'return an empty array',
+    actual: 1,
+    expected: 1
+  })
+})
