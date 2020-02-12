7cc3c6e7555751b42c06b9c5f0cbb55022562314
Ryan Scharfer <ryan.scharfer@finanzcheck.de>
2020-02-12 16:36:00 +0100

    testing automatic deploymentz

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { questionsReducer } from "../src/reducers/questions-reducer";
 
 function MyApp({ Component, pageProps }) {
   // wrap every 'page' component with <Provider/> so that it can connect to the Redux store
+  console.log('testing automatic deployment');
   return (
     <Provider store={createStore(questionsReducer)}>
       <Component {...pageProps} />
